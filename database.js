46281cf3690781e998666b3c4291d64d9ec03ec4
theflyoccultist <y.delahaije@gmail.com>
2024-08-14 23:15:30 +0200

    first commit

--- database.js
+++ database.js
@@ -1,11 +1,16 @@
 import mysql from 'mysql2'
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'brat',
-    database: 'todolist'
+    host: process.env.MYSQl_HOST,
+    user: process.env.MYSQl_USER,
+    password: process.env.MYSQl_PASSWORD,
+    database: process.env.MYSQl_DATABASE
 }).promise()
 
-const result = await pool.query("SELECT * FROM list")
-console.log(result)
+async function getTasks() {
+    const [rows] = await pool.query("SELECT * FROM list")
+    return rows
+}
+
+const tasks = await getTasks()
+console.log(tasks)
